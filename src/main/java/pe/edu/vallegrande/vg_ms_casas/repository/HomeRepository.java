3f6e4df2a9cfed341f3c7f6470c8d6b6fd4f7d23
jonas Zubieta león <89934911+jonaszubietaleon@users.noreply.github.com>
2025-07-05 06:30:46 +0000

    cambios en  consumo interno

--- src/main/java/pe/edu/vallegrande/vg_ms_casas/repository/HomeRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.vallegrande.vg_ms_casas.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface HomeRepository extends ReactiveCrudRepository<Home, Integer> {
-    Flux<Home> findByStatus(String status);
-}
